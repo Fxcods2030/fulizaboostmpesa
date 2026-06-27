ed43ed593b5d987c4d859c1c3bcff38dc620f95d
Fxcods2030 <fuliza@lookafricasafaris.co.ke>
2026-06-27 13:18:29 +0300

    Update pay.js

--- api/pay.js
+++ api/pay.js
@@ -8,9 +8,9 @@ export default async function handler(req, res) {
     const { phone, amount, reference } = req.body;
     if (!phone || !amount) return res.status(400).json({ error: 'Phone and amount required' });
 
-    const FLOWCASH_API_KEY = 'b88a96eb72bd145c8ab02d56b8d08d7cae9c5d1e9451b7ee002797640123af9e';
-    const FLOWCASH_EMAIL = 'elishakoskey36@gmail.com';
-    const FLOWCASH_ENDPOINT = 'https://flowcash.co.ke/v1/stkpush'; // ← CORRECT ENDPOINT
+     const MEGAPAY_API_KEY = 'MGPYUXJqx4yT';
+     const MEGAPAY_EMAIL = 'elishakoskey36@gmail.com';
+     const MEGAPAY_ENDPOINT = 'https://megapay.co.ke/backend/v1/initiatestk';// ← CORRECT ENDPOINT
 
     try {
         const response = await fetch(FLOWCASH_ENDPOINT, {
